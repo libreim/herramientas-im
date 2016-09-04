094cbf7fb26074a7eeffc0680c88ade21e6c2174
David Charte <fdavidcl@outlook.com>
2016-09-04 20:57:38 +0200

    Demo básica de Git/Github

--- demos/git-github.sh
+++ demos/git-github.sh
@@ -13,11 +13,20 @@ exit 0
 # - Activa la verificación en dos pasos (opcional pero recomendable)
 # - Crea un nuevo repositorio <https://github.com/new> (inícialo con un README)
 
-# - Crea una copia local en tu ordenador:
+# - Crea una copia local en tu ordenador
 git clone https://github.com/tu_usuario/tu_repo.git
 cd tu_repo
 
 # - Crea y modifica archivos
 echo "Hello world" > test.txt
 
-# - 
+echo "Repositorio de prueba. Modificaciones por $(whoami)" >> README.md
+
+# - Añade los cambios a un nuevo commit
+git add test.txt README.md
+git commit "Pequeñas modificaciones
+
+Añade un nuevo archivo y una aclaración en el Readme."
+
+# - Manda los nuevos cambios al remoto
+git push origin master
